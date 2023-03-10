b2c8ba3d14db5ed7c9736bc1fdaa3f2c2ecb2edb
dudu <whqtkf12@naver.com>
2023-03-10 16:01:01 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by dudu on 2023/03/10.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
