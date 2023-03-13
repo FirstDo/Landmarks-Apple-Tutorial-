5dbdf3f977d1b6048905838f77e42e93ef642fda
dudu <whqtkf12@naver.com>
2023-03-13 12:26:42 +0900

    feat: LandmarkList 구현

---
+++ Landmarks/LandmarkList.swift
@@ -0,0 +1,22 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by dudu on 2023/03/13.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List(landmarks) { landmark in
+            LandmarkRow(landmark: landmark)
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
